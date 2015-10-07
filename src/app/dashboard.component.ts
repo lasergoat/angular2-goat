07051395e08b4ccd657edc202ed73115c97295f9
David Khourshid <davidk@counsyl.com>
2015-10-06 17:00:13 -0700

    oops

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -3,25 +3,27 @@ import {Component, FORM_DIRECTIVES, View} from 'angular2/angular2';
 @Component({ selector: 'my-dashboard' })
 @View({
 	template: `
-		<header class="t-header"></header>
-		<main class="t-main">
-		  <div class="t-goats">
-		    <!-- goats go here -->
-		    <div class="t-goat">
-		      <img src="" alt="" class="t-photo">
-		      <div class="t-info">
-		        <span class="t-name">Billy</span>
-		        <span class="t-age">20</span>
+		<div class="t-app">
+		  <header class="t-header"></header>
+		  <main class="t-main">
+		    <div class="t-goats">
+		      <!-- goats go here -->
+		      <div class="t-goat">
+		        <img src="" alt="" class="t-photo">
+		        <div class="t-info">
+		          <span class="t-name"></span>
+		          <span class="t-age"></span>
+		        </div>
 		      </div>
+		  
 		    </div>
-
-		  </div>
-		  <div class="t-buttons">
-		    <button class="t-button dislike"></button>
-		    <button class="t-button info"></button>
-		    <button class="t-button like"></button>
-		  </div>
-		</main>
+		    <div class="t-buttons">
+		      <button class="t-button dislike"></button>
+		      <button class="t-button info"></button>
+		      <button class="t-button like"></button>
+		    </div>
+		  </main>
+		</div>
 	`,
 	directives: [FORM_DIRECTIVES]
 })
