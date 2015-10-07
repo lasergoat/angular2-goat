ec5b52e70ceacc1ad97bcc1e20676a7d9de72be3
Daniel Walker <daniel@fattmerchant.com>
2015-10-06 20:08:37 -0400

    make goat service and add markup to dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,15 @@
 import {View, Component} from 'angular2/angular2';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
-import {CharactersComponent} from './characters.component';
 import {DashboardComponent} from './dashboard.component';
 
 @Component({ selector: 'my-app' })
 @View({
   template: `
-    <a [router-link]="['./Dashboard']">Dashboard</a>
-    <a [router-link]="['./Characters']">Characters</a>
     <router-outlet></router-outlet>
     `,
   directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
-  { path: '/', as: 'Dashboard', component: DashboardComponent },
-  { path: '/characters', as: 'Characters', component: CharactersComponent }
+  { path: '/', as: 'Dashboard', component: DashboardComponent }
 ])
 export class AppComponent { }
